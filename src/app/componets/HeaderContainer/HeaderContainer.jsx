411f7fa7b2f1603ab26bb7ad5f1a5dd76ba1f8c9
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-10-09 06:00:11 +0500

    new

--- src/app/componets/HeaderContainer/HeaderContainer.jsx
+++ src/app/componets/HeaderContainer/HeaderContainer.jsx
@@ -8,7 +8,7 @@ export default function HeaderContainer() {
             <div className={Style.MainCol}>
                 <Profile />
             </div>
-            <div className={Style.MainCol}>
+            <div className={`${Style.MainCol} hidden md:block`}>
                 <h1 className={Style.MainTitle}>Portfolio</h1>
             </div>
         </div>
